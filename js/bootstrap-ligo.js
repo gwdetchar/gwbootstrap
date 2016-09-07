dc81c11e8956f5a7498b66356de9784b8b0f9ee0
Duncan Macleod <duncan.macleod@ligo.org>
2016-09-07 16:25:22 -0500

    footer: dynamically resize footer to match content
    on small displays, footer will wrap around and look crappy

--- js/bootstrap-ligo.js
+++ js/bootstrap-ligo.js
@@ -1,10 +1,27 @@
-// match page position to navbar height
+/** match page position to navbar height */
 function matchPageTopToNavbar() {
   $('.navbar-fixed-top + .container').css('padding-top', $('header').height());
 }
+
+/** match footer height to content */
+function matchFooterHeight() {
+  // get container dimensions
+  var newheight = $('.footer > .container').outerHeight();
+  // reset footer height
+  $footer = $('.footer');
+  fpad = $footer.outerHeight() - $footer.height();
+  $footer.height(newheight + fpad);
+  // reset body margin
+  padtop = parseInt($footer.css('padding-top'));
+  padbottom = parseInt($footer.css('padding-bottom'));
+  $('body').css('margin-bottom', $footer.outerHeight() + padtop + padbottom);
+}
+
 $(window).load(function() {
   matchPageTopToNavbar();
+  matchFooterHeight();
 });
 $(window).resize(function() {
   matchPageTopToNavbar();
+  matchFooterHeight();
 });
