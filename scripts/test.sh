c26b561f4744f676ef8c34b2f1dfc1dd64dbe8f5
Alex Urban <alexander.urban@ligo.org>
2019-10-16 14:43:13 -0500

    Refactor JavaScript utils and attempt to patch lazy image loading

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,7 @@ TESTDIR="test"
 mkdir -p ${TESTDIR}
 
 # JavaScript files
+COREJS="js/_core.js"
 LIGHTJS="js/gwbootstrap.js"
 EXTRAJS="js/gwbootstrap-extra.js"
 LIGHTJS_OUT="${TESTDIR}/gwbootstrap.min.js"
@@ -16,9 +17,9 @@ CSSOUT="${TESTDIR}/gwbootstrap.min.css"
 
 # get babel and sass commands
 BABELCMD="babel --no-comments --compact --minified"
-CMD1="${BABELCMD} --out-file ${LIGHTJS_OUT} ${LIGHTJS}"
-CMD2="${BABELCMD} --out-file ${EXTRAJS_OUT} ${LIGHTJS} ${EXTRAJS}"
-CMD3="sass --style compressed ${CSSIN} ${CSSOUT}"
+CMD1="${BABELCMD} --out-file ${LIGHTJS_OUT} ${COREJS} ${LIGHTJS}"
+CMD2="${BABELCMD} --out-file ${EXTRAJS_OUT} ${COREJS} ${EXTRAJS}"
+CMD3="sass --style compressed --no-source-map ${CSSIN} ${CSSOUT}"
 
 # build minified stylesheets and scripts
 declare -a Commands=("${CMD1}" "${CMD2}" "${CMD3}")
